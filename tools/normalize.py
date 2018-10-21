0b078af84e3d8e0684d355700e1dff2caf2deda1
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2018-10-21 11:13:22 +0330

    added DS-S09-2018-10-20--10-30-00.mp3

--- tools/normalize.py
+++ tools/normalize.py
@@ -9,7 +9,8 @@ import subprocess
 import re
 import shutil
 
-FILES_PATTERN = 'rec-*.mp3'
+MP3_REC_PATTERN = 'rec-*.mp3'
+M4A_REC_PATTERN = 'rec-*.m4a'
 SOURCE_FILE_PREFIX_CHAR_COUNT = 4
 DEST_FILE_PREFIX = 'voice-'
 
@@ -18,8 +19,9 @@ if __name__ == '__main__':
         pipe_null = 'NUL'
     else:
         pipe_null = '/dev/null'
-    files = glob.glob(FILES_PATTERN)
 
+    files = glob.glob(MP3_REC_PATTERN)
+    files.extend(glob.glob(M4A_REC_PATTERN))
     for source_path in files:
         bare_path = source_path[SOURCE_FILE_PREFIX_CHAR_COUNT:]
         filtered_path = 'filtered-' + bare_path
@@ -28,6 +30,17 @@ if __name__ == '__main__':
             continue
         subprocess.call('ffmpeg -i "%s" -af "highpass=f=200, lowpass=f=3000" -n "%s"' % (source_path, filtered_path),
             stderr=subprocess.STDOUT, shell=True)
+        if source_path[-4:].lower() == '.m4a':
+            bare_path_mp3 = source_path[SOURCE_FILE_PREFIX_CHAR_COUNT:-4] + '.mp3'
+            filtered_path_mp3 = 'filtered-' + bare_path_mp3
+            voice_path_mp3 = DEST_FILE_PREFIX + bare_path_mp3
+            subprocess.call('ffmpeg -i "%s" -vn -sn -dn -c:v copy -b:a 24k -ar 16000 -f mp3 "%s"' % (filtered_path, filtered_path_mp3))
+            try:
+                os.remove(filtered_path)
+            except:
+                pass
+            filtered_path = filtered_path_mp3
+            voice_path = voice_path_mp3
         vol_output = subprocess.check_output('ffmpeg -i "%s" -af "volumedetect" -vn -sn -dn -f null %s' % (filtered_path, pipe_null), 
             stderr=subprocess.STDOUT, shell=True).decode()
         vol = None
